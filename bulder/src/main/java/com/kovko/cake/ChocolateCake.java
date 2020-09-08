792fd203e1ee90cfd87db313cd4b5f3b92187bf2
eu.kovko <eu.kovko@gmail.com>
2020-09-08 15:14:56 +0300

    Builder implementation

---
+++ bulder/src/main/java/com/kovko/cake/ChocolateCake.java
@@ -0,0 +1,13 @@
+package com.kovko.cake;
+
+/**
+ * Author: eukovko
+ * Date: 9/8/2020
+ */
+public class ChocolateCake extends AbstractCake {
+
+    public ChocolateCake() {
+
+        setName("Chocolate cake");
+    }
+}
